f001c60b28941e4d1fadae749655b0373a4f0e68
Oleksandr-Berd <alex.berd86@gmail.com>
2025-06-22 16:30:18 +0300

    discover is done

--- src/components/Discover/Discover.jsx
+++ src/components/Discover/Discover.jsx
@@ -4,20 +4,20 @@ const Discover = () => {
     return ( 
         <SC.DiscoverConStyled id="discover">
             <ul>
-                <li>
+                <SC.DiscoverItem>
                     <h3>$89,914</h3>
                     <p>
                         of $100,000 backed
                     </p>
-                </li>
-                <li>
+                </SC.DiscoverItem>
+                <SC.DiscoverItem>
                     <h3>5,007</h3>
                     <p>total backers</p>
-                </li>
-                <li>
+                </SC.DiscoverItem>
+                <SC.DiscoverItem>
                     <h3>56</h3>
                     <p>days left</p>
-                </li>
+                </SC.DiscoverItem>
             </ul>
             <div></div>
         </SC.DiscoverConStyled>
--- src/components/Discover/DiscoverStyled.js
+++ src/components/Discover/DiscoverStyled.js
@@ -1,4 +1,57 @@
 import styled from "@emotion/styled";
 
 export const DiscoverConStyled = styled.div`
-`
+  padding-top: 32px;
+  padding-bottom: 32px;
+  padding-left: 24px;
+  padding-right: 24px;
+
+  text-align: center;
+
+  background-color: #fff;
+
+  border-radius: 8px;
+`;
+
+export const DiscoverItem = styled.li`
+  position: relative;
+
+  padding-top: 24px;
+  &::after {
+    content: "";
+
+    position: absolute;
+
+    bottom: 0;
+    left: 50%;
+
+    transform: translate(-50%, 24px);
+
+    width: 80px;
+    height: 2px;
+
+    display: inline-block;
+
+    background-color: #979797;
+
+    opacity: 0.5;
+  }
+
+  &:not(:last-of-type) {
+    margin-bottom: 24px;
+  }
+
+  & > h3 {
+    margin-bottom: 8px;
+
+    font-size: 32px;
+    font-weight: 700;
+
+    color: #000;
+  }
+
+  & > p {
+    font-size: 14px;
+    color: #7a7a7a;
+  }
+`;
